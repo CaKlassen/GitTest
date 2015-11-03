f81db6b4324b1201f431d8291719536e471d146b
Chris Klassen <ca.klassen95@gmail.com>
2015-11-02 19:45:53 -0800

    Added test files.

---
+++ app/src/main/java/com/example/chris/gittest/Helper.java
@@ -0,0 +1,7 @@
+package com.example.chris.gittest;
+
+/**
+ * Created by Chris on 11/2/2015.
+ */
+public class Helper {
+}
--- app/src/main/java/com/example/chris/gittest/MainActivity.java
+++ app/src/main/java/com/example/chris/gittest/MainActivity.java
@@ -11,6 +11,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    // This is a comment
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
